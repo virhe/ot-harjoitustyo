1660b1542d7a871348e586b832673fb597b2b266
tuome <tonni@tuta.io>
2023-11-14 22:16:56 +0200

    Add week 2 exercise 8

---
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -0,0 +1,81 @@
+import unittest
+from kassapaate import Kassapaate
+from maksukortti import Maksukortti
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassapaate = Kassapaate()
+        self.kortti = Maksukortti(1000)
+    
+    def test_kassapaate_alkuarvot(self):
+        self.assertEqual(self.kassapaate.kassassa_rahaa_euroina(), 1000)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_kateisosto_kassapaate_kasvaa(self):
+        self.kassapaate.syo_maukkaasti_kateisella(600)
+
+        self.assertEqual(self.kassapaate.kassassa_rahaa_euroina(), 1004)
+    
+    def test_kateisosto_vaihtoraha(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(600), 200)
+
+    def test_kateisosto_lounaiden_maara_nousee(self):
+        self.kassapaate.syo_maukkaasti_kateisella(400)
+
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_kateisosto_vaara_kasvaa(self):
+        self.kassapaate.syo_maukkaasti_kateisella(200)
+
+        self.assertEqual(self.kassapaate.kassassa_rahaa_euroina(), 1000)
+    
+    def test_kateisosto_vaara_palautus(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(200), 200)
+
+    def test_kateisosto_vaara_lounaiden_maara(self):
+        self.kassapaate.syo_maukkaasti_kateisella(200)
+
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_korttiosto_veloitus(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 6)
+
+    def test_korttiosto_true(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.kortti), True)
+
+
+    def test_korttiosto_lounaiden_maara_nousee(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_korttiosto_vaara_rahamaara_ei_muuto(self):
+        self.kortti = Maksukortti(100)
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 1)
+
+    def test_korttiosto_vaara_rahamaara_false(self):
+        self.kortti = Maksukortti(100)
+
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.kortti), False)
+
+    def test_korttiosto_vaara_rahamaara_maara_ei_muutu(self):
+        self.kortti = Maksukortti(100)
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+    
+    def test_kassa_rahamaara_ei_muutu(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+
+        self.assertEqual(self.kassapaate.kassassa_rahaa_euroina(), 1000)
+
+    def test_rahan_lataaminen_rahamaara(self):
+        self.kassapaate.lataa_rahaa_kortille(self.kortti, 100)
+
+        self.assertEqual(self.kassapaate.kassassa_rahaa_euroina(), 1001)
+        self.assertEqual(self.kortti.saldo_euroina(), 11)
