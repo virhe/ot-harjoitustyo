e7eab4e32ea96ffc9d805b8b67fa38bf1a44e80e
tuome <tonni@tuta.io>
2023-11-21 23:48:18 +0200

    Add main.py main function

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from entities.character import Character
 from entities.player_class import Barbarian
 from entities.race import Human
 
-pclass = Barbarian()
-prace = Human()
 
-player_character = Character("Example", pclass, prace)
+def main():
+    pclass = Barbarian()
+    prace = Human()
 
-print(player_character)
+    player_character = Character("Example", pclass, prace)
+
+    print(player_character)
+
+
+if __name__ == "__main__":
+    start()
