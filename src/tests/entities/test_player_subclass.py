cc941c524caf40367db1ba41f89c0cde22a107bb
tuome <tuomela.onni@gmail.com>
2024-04-09 15:05:52 +0300

    Initial project layout

--- src/tests/entities/test_player_subclass.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from entities.player_class import PlayerClass
-
-
-class TestPlayerClass(unittest.TestCase):
-    def test_player_class_can_be_initialized(self):
-        player_class = PlayerClass("Testi", list("testi"), list("testi"))
-        assert player_class is not None
