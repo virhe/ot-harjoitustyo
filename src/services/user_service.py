406ddd4201e307877d35a30add25e2abe09343a3
tuome <tuomela.onni@gmail.com>
2024-04-16 10:12:59 +0300

    Implement login and register in UserService

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,9 +1,26 @@
+import bcrypt
+
+from src.entities.user import User
+
+
 class UserService:
     def __init__(self, user_repository):
         self.user_repository = user_repository
 
     def login(self, username, password):
-        pass
+        user = self.user_repository.find_user_name(username)
+        if user and bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+            return True
+
+        return False
 
     def register(self, username, password):
-        pass
+        if self.user_repository.find_user_name(username):
+            raise Exception("User with given username already exists")
+
+        user = User(username=username, password=hash_pw(password))
+        self.user_repository.add_user(user)
+
+
+def hash_pw(password):
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
