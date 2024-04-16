b9082bd21fa5d36a3c47ab7009728d8d663ab5d3
tuome <tuomela.onni@gmail.com>
2024-04-16 09:21:35 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 import tkinter as tk
 
-root = tk.Tk()
+from src.gui.window import MainWindow
+from src.services.user_service import UserService
 
-root.title("Depysit")
 
-root.mainloop()
+def main():
+    root = tk.Tk()
+
+    user_service = UserService()
+
+    app = MainWindow(root, user_service)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
