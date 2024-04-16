944708a5aa817f4c251d4564d82093fcfe61f52a
tuome <tuomela.onni@gmail.com>
2024-04-16 10:11:09 +0300

    Remove ComboForm from window.py

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import Toplevel
 
 
 class MainWindow:
@@ -14,30 +13,3 @@ class MainWindow:
 
     def create_ui(self):
         tk.Label(self.root, text="Welcome to Depysit").pack()
-
-
-class ComboForm(Toplevel):
-    def __init__(self, root, user_service):
-        super().__init__(root)
-        self.user_service = user_service
-        self.title("Login/Register")
-
-        tk.Label(self, text="Username:").pack()
-        self.username = tk.Entry(self)
-        self.username.pack()
-
-        tk.Label(self, text="Password:").pack()
-        self.password = tk.Entry(self, show="*")
-        self.password.pack()
-
-        login_button = tk.Button(self, text="Login", command=self.login)
-        login_button.pack()
-
-        register_button = tk.Button(self, text="Register", command=self.register)
-        register_button.pack()
-
-    def login(self):
-        pass
-
-    def register(self):
-        pass
