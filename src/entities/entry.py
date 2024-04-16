1dd169a951651fc2d700e7659bc56b2918d26782
tuome <tuomela.onni@gmail.com>
2024-04-16 03:56:16 +0300

    Add Entry entity

---
+++ src/entities/entry.py
@@ -0,0 +1,7 @@
+
+class Entry:
+    def __init__(self, amount, category, description, date):
+        self.amount = amount
+        self.category = category
+        self.description = description
+        self.date = date
