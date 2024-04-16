1f08682c90eece96ae8757f4c6c3e01e7e7a0195
tuome <tuomela.onni@gmail.com>
2024-04-16 11:59:54 +0300

    Add EntryForm to main window

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 
+from src.gui.entry_form import EntryForm
+
 
 class MainWindow:
-    def __init__(self, root):
+    def __init__(self, root, entry_service, user_id):
         self.root = root
+        self.entry_service = entry_service
+        self.user_id = user_id
         self.root.title("Depysit")
         self.root.geometry("800x600")
 
@@ -11,3 +15,10 @@ class MainWindow:
 
     def create_ui(self):
         tk.Label(self.root, text="Welcome to Depysit").pack()
+
+        add_entry = tk.Button(self.root, text="Add Entry", command=self.entry_form)
+        add_entry.pack()
+
+
+    def entry_form(self):
+        entry_form = EntryForm(self.root, self.entry_service, self.user_id)
