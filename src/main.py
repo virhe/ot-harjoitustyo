c164bd449994880401843d78fe53d14d685e9711
tuome <tuomela.onni@gmail.com>
2024-04-16 10:13:25 +0300

    Launch ComboForm in main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 import tkinter as tk
 
-from src.gui.window import MainWindow
+from src.gui.form import ComboForm
+from src.repositories.entry_repository import EntryRepository
+from src.repositories.user_repository import UserRepository
 from src.services.user_service import UserService
+from src.services.entry_service import EntryService
+from src.database import init_db
 
 
 def main():
+    init_db()
+
+    user_repository = UserRepository()
+    entry_repository = EntryRepository()
+
+    user_service = UserService(user_repository)
+    entry_service = EntryService(entry_repository)
+
     root = tk.Tk()
-
-    user_service = UserService()
-
-    app = MainWindow(root, user_service)
+    form = ComboForm(root, user_service)
 
     root.mainloop()
 
