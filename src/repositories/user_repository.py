8b84e2390b97a1273d51f4aa3cdded6ca08f8867
tuome <tuomela.onni@gmail.com>
2024-04-16 10:11:59 +0300

    Add search by username to UserRepository

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -11,10 +11,14 @@ class UserRepository:
         self.session.commit()
 
     def delete_user(self, user_id):
-        user = self.get_user_id(user_id)
+        user = self.find_user_id(user_id)
         self.session.delete(user)
         self.session.commit()
 
-    def get_user_id(self, user_id):
+    def find_user_id(self, user_id):
         user = self.session.query(User).filter_by(id=user_id).first()
+        return user
+
+    def find_user_name(self, username):
+        user = self.session.query(User).filter_by(username=username).first()
         return user
