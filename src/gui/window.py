3e653641ab6b4bbf5a49174a9e67b2ec02de314a
tuome <tuomela.onni@gmail.com>
2024-04-16 13:03:47 +0300

    Add treeview of entries and refresh button

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from src.gui.entry_form import EntryForm
+from tkinter import ttk, messagebox
 
 
 class MainWindow:
@@ -16,9 +17,41 @@ class MainWindow:
     def create_ui(self):
         tk.Label(self.root, text="Welcome to Depysit").pack()
 
+        # Has to be instance variable for entry_form()
+        self.tree = ttk.Treeview(self.root)
+        self.tree["columns"] = ("ID", "Amount", "Category", "Date", "Description")
+        self.tree.column("#0", width=0, stretch=tk.NO)
+        self.tree.column("ID", anchor=tk.W, width=40)
+        self.tree.column("Amount", anchor=tk.W, width=80)
+        self.tree.column("Category", anchor=tk.W, width=120)
+        self.tree.column("Date", anchor=tk.W, width=120)
+        self.tree.column("Description", anchor=tk.W, width=200)
+
+        self.tree.heading("#0", text="", anchor=tk.W)
+        self.tree.heading("ID", text="ID", anchor=tk.W)
+        self.tree.heading("Amount", text="Amount", anchor=tk.W)
+        self.tree.heading("Category", text="Category", anchor=tk.W)
+        self.tree.heading("Date", text="Date", anchor=tk.W)
+        self.tree.heading("Description", text="Description", anchor=tk.W)
+
+        self.tree.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
         add_entry = tk.Button(self.root, text="Add Entry", command=self.entry_form)
         add_entry.pack()
 
+        refresh_button = tk.Button(self.root, text="Refresh", command=self.refresh)
+        refresh_button.pack()
+
+        self.refresh()
 
     def entry_form(self):
         entry_form = EntryForm(self.root, self.entry_service, self.user_id)
+
+    def refresh(self):
+        for entry in self.tree.get_children():
+            self.tree.delete(entry)
+
+        entries = self.entry_service.entries_by_user(self.user_id)
+        for entry in entries:
+            self.tree.insert("", "end", values=(entry.id, entry.amount,
+                                                entry.category, entry.date, entry.description))
