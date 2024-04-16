50a69b27405c04b9380a801283b727303764d044
tuome <tuomela.onni@gmail.com>
2024-04-16 04:31:45 +0300

    Add docstring to Entry

--- src/entities/entry.py
+++ src/entities/entry.py
@@ -1,6 +1,14 @@
 
 class Entry:
-    """ A class to represent a budgeting Entry """
+    """ A class to represent a budgeting Entry
+
+     Attributes:
+         amount (float): The amount of currency
+         category (str): The category of the entry
+         description (str): The description of the entry
+         date (str): The date of the entry
+    """
+
     def __init__(self, amount: float, category: str, description: str, date: str) -> None:
         self.amount = amount
         self.category = category
