db7dc4125e9ec95084d9b72d8f045b9d88c63592
tuome <tuomela.onni@gmail.com>
2024-04-16 04:32:51 +0300

    Add more type hinting to Entry

--- src/entities/entry.py
+++ src/entities/entry.py
@@ -10,7 +10,7 @@ class Entry:
     """
 
     def __init__(self, amount: float, category: str, description: str, date: str) -> None:
-        self.amount = amount
-        self.category = category
-        self.description = description
-        self.date = date
+        self.amount: float = amount
+        self.category: str = category
+        self.description: str = description
+        self.date: str = date
