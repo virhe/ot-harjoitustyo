816d7d70ab1c14231083f1711a26985d62bf285c
tuome <tuomela.onni@gmail.com>
2024-04-16 09:21:22 +0300

    Add intial services

---
+++ src/services/user_service.py
@@ -0,0 +1,9 @@
+class UserService:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+
+    def login(self, username, password):
+        pass
+
+    def register(self, username, password):
+        pass
