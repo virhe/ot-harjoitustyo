e13753b837140993794bef6eae218f16803f58d1
tuome <tuomela.onni@gmail.com>
2024-04-30 14:13:29 +0300

    Update user_service.py dosctrings

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -4,18 +4,30 @@ from src.entities.user import User
 
 
 class UserService:
-    """
-    Responsible for managing all users.
-
-    Attributes:
-        user_repository: Handles database operations for all users.
+    """Responsible for the logic related to users
     """
 
     def __init__(self, user_repository):
+        """Constructor
+
+        Args:
+            user_repository: user_repository instance
+        """
+
         self.user_repository = user_repository
 
-    # Handles user login
     def login(self, username, password):
+        """Handles user login
+
+        Args:
+            username: username of the user
+            password: password of the user
+
+        Returns:
+            user.id: id of the user (on success)
+            None: on failure
+        """
+
         user = self.user_repository.find_user_name(username)
         if user and bcrypt.checkpw(
             password.encode("utf-8"), user.password.encode("utf-8")
@@ -24,8 +36,21 @@ class UserService:
 
         return None
 
-    # Handles user registration
     def register(self, username, password):
+        """Handles user registration
+
+        Args:
+            username: username of the user
+            password: password of the user
+
+        Raises:
+            UsernameTakenError: if the username is already taken
+            InvalidUsernameOrPassword: if the username or password is under 3 characters long
+
+        Returns:
+            user.id: id of the added user
+        """
+
         if self.user_repository.find_user_name(username):
             raise UsernameTakenError("User with given username already exists")
 
@@ -40,16 +65,27 @@ class UserService:
         return self.user_repository.add_user(user)
 
 
-# Hashes the password
-# Not a course requirement, but good practice
+# Not a course requirement, but a good practice
 def hash_pw(password):
+    """Hashes the given password
+
+    Args:
+        password: password to be hashed
+
+    Returns:
+        Hashed password
+    """
+
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 
-# Custom errors so pylint won't complain about "Exception"
 class UsernameTakenError(Exception):
+    """Custom exception for when a username is taken
+    """
     pass
 
 
 class InvalidUsernameOrPassword(Exception):
+    """Custom exception for when a username or password is under 3 characters long
+    """
     pass
