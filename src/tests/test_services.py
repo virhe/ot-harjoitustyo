967c4915b10c67a60040bb8f617e6fdcf249198a
tuome <tuomela.onni@gmail.com>
2024-04-30 13:29:00 +0300

    Add tests for user_service.py

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -1,9 +1,11 @@
 from unittest.mock import MagicMock
 
+import bcrypt
 import pytest
 
 from src.services.entry_service import EntryService
-from src.services.user_service import UserService
+from src.services.user_service import UserService, UsernameTakenError, InvalidUsernameOrPassword
+from src.entities.user import User
 
 
 # Fixtures
@@ -44,4 +46,40 @@ def test_entries_by_user(entry_service, entry_repository):
 # -----
 
 
-# TODO user_service tests
+# UserService
+def test_login(user_service, user_repository):
+    password = bcrypt.hashpw("loginpass".encode("utf-8"), bcrypt.gensalt()).decode('utf-8')
+    user_repository.find_user_name.return_value = User(id=1, username="loginuser", password=password)
+
+    assert user_service.login("loginuser", "loginpass") == 1
+
+
+def test_wrong_login(user_service, user_repository):
+    password = bcrypt.hashpw("loginpass".encode("utf-8"), bcrypt.gensalt()).decode('utf-8')
+    user_repository.find_user_name.return_value = User(id=1, username="loginuser", password=password)
+
+    assert user_service.login("loginuser", "incorrectloginpass") is None
+
+
+def test_register(user_service, user_repository):
+    user_repository.find_user_name.return_value = None
+    user_repository.add_user.return_value = 1
+
+    assert user_service.register("registeruser", "registerpass") == 1
+
+
+def test_register_username_taken(user_service, user_repository):
+    user_repository.find_user_name.return_value = User(id=1, username="takenname", password="takenpass")
+
+    with pytest.raises(UsernameTakenError):
+        user_service.register("takenname", "takenpass")
+
+
+def test_register_bad_input(user_service, user_repository):
+    user_repository.find_user_name.return_value = None
+
+    with pytest.raises(InvalidUsernameOrPassword):
+        user_service.register("a", "b")
+
+
+# -----
